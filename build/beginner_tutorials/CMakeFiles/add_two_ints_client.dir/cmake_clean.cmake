16304756f5c8f05e2cbdfc06e608993b218a7917
savio23 <aditya.savio23@gmail.com>
2019-02-28 15:22:20 +0200

    ROS Begin01

---
+++ build/beginner_tutorials/CMakeFiles/add_two_ints_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add_two_ints_client.dir/src/add_two_ints_client.cpp.o"
+  "/home/savio/adityasaviopaul-rtech/devel/lib/beginner_tutorials/add_two_ints_client.pdb"
+  "/home/savio/adityasaviopaul-rtech/devel/lib/beginner_tutorials/add_two_ints_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add_two_ints_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/savio/adityasaviopaul-rtech/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/savio/adityasaviopaul-rtech/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
