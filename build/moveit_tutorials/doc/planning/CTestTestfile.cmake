55f55debdd739e1bf0ade0cbec5d9ed736866119
savio23 <aditya.savio23@gmail.com>
2019-04-26 13:03:45 +0300

    Panda Arm Motion Planning

---
+++ build/moveit_tutorials/doc/planning/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/savio/adityasaviopaul-rtech/src/moveit_tutorials/doc/planning
+# Build directory: /home/savio/adityasaviopaul-rtech/build/moveit_tutorials/doc/planning
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/planning_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/savio/adityasaviopaul-rtech/src/planning_tutorial
+# Build directory: /home/savio/adityasaviopaul-rtech/build/planning_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
