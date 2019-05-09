dc5ba4c30a834fa2ad0d22995913c7d4949b451d
savio23 <aditya.savio23@gmail.com>
2019-05-09 15:10:50 +0300

    Robot Manipulator

--- src/planning_tutorial/src/robotmanipulator.cpp
+++ src/planning_tutorial/src/robotmanipulator.cpp
@@ -79,41 +79,7 @@ int main(int argc, char** argv)
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  // We will use the :planning_scene_interface:`PlanningSceneInterface`
-  // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const robot_state::JointModelGroup* joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-	
-  // Visualization
-  // ^^^^^^^^^^^^^
-  //
-  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("tool0");
-  visual_tools.deleteAllMarkers();
-
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in RViz
-  visual_tools.loadRemoteControl();
-
-
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-
-  // Planning with constraints can be slow because every sample must call an inverse kinematics solver.
-  // Lets increase the planning time from the default 5 seconds to be sure the planner has enough time to succeed.
-  visual_tools.prompt("Click Next to Proceed");
-
-  // When done with the path constraint be sure to clear it.
-  move_group.clearPathConstraints();
-
-  // Since we set the start state we have to clear it before planning other paths
-  move_group.setStartStateToCurrentState();
-
+ 
   // Cartesian Paths
   // ^^^^^^^^^^^^^^^
   // You can plan a Cartesian path directly by specifying a list of waypoints
@@ -124,50 +90,38 @@ int main(int argc, char** argv)
 
   std::vector<geometry_msgs::Pose> waypoints;
   waypoints.push_back(target_pose3);
-
+  
+	//Move Robot Down
   target_pose3.position.z -= 0.2;
   target_pose3.position.y += 0.2;
+  waypoints.push_back(target_pose3); 
 
-  waypoints.push_back(target_pose3);  // down
-
+	//Move Robot Right
   target_pose3.position.y -= 0.4;
-  waypoints.push_back(target_pose3);  // right
+  waypoints.push_back(target_pose3);  
 
+	//Move Robot Upwards and Left
   target_pose3.position.z += 0.2;
   target_pose3.position.y += 0.2;
-  //target_pose3.position.x -= 0.2;
-  waypoints.push_back(target_pose3);  // up and left
+  waypoints.push_back(target_pose3);  
 
 
-  // Cartesian motions are frequently needed to be slower for actions such as approach and retreat
-  // grasp motions. Here we demonstrate how to reduce the speed of the robot arm via a scaling factor
-  // of the maxiumum speed of each joint. Note this is not the speed of the end effector point.
-  move_group.setMaxVelocityScalingFactor(0.1);
 
   // We want the Cartesian path to be interpolated at a resolution of 1 cm
   // which is why we will specify 0.01 as the max step in Cartesian
   // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
   // Warning - disabling the jump threshold while operating real hardware can cause
   // large unpredictable motions of redundant joints and could be a safety issue
+  
   moveit_msgs::RobotTrajectory trajectory;
   const double jump_threshold = 0.0;
   const double eef_step = 0.01;
-  double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+  move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
 
 
-  // Visualize the plan in RViz
-  visual_tools.deleteAllMarkers();
-
-  for (std::size_t i = 0; i < waypoints.size(); ++i)
-visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::SMALL);
-  visual_tools.trigger();
 
 my_plan.trajectory_=trajectory;
 move_group.execute(my_plan);
-
-
-
-  // END_TUTORIAL
 
   ros::shutdown();
   return 0;
